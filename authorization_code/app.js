3f124b77e5d211f83ca1e862a6bcea0b3c6fd459
pennetti <tjp7mb@virginia.edu>
2014-09-09 22:06:04 -0400

    retrieved artists and sorted by number of occurrences, need separate call for artist genres

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
     }));
 });
 
-app.all('/callback', function(req, res) {
+app.get('/callback', function(req, res) {
 
   // your application requests refresh and access tokens
   // after checking the state parameter
